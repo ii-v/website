17482deecf155c18725f60a83dda2222e18ea14a
ii-v <iiv@openmailbox.org>
2016-08-02 20:59:37 +0200

    added lists and templates. Most static content is in place

--- start.py
+++ start.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 from flask import *
+import markdown
 app = Flask(__name__)
-things_i_like = "test"
-projects = "test2"
+things_i_like = open('things_i_like.list', 'r').readlines()
+projects = open('projects.list', 'r').readlines()
 @app.route('/')
 def start_page():
-    return render_template('./index.html', things_i_like=things_i_like, projects=projects)
+    return render_template('index.html', things_i_like=things_i_like, projects=projects)
     
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello, {}".format(name)
+@app.route('/<number>')
+def hello_name(number):
+    return "{0} in binary is {1}".format(number, bin(number))
